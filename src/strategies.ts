678700db91bf21b4741046d56d9149e47a1a4093
Edward Irby <e.irby@pm.me>
2021-05-29 18:18:01 -0700

    ci: security updates

--- src/strategies.ts
+++ src/strategies.ts
@@ -1,28 +1,26 @@
 import {CandidateBid} from './types'
+
+/** @description Randomized Priority Queue Selection Strategy */
 const shuffle = (array: unknown[]) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     ;[array[i], array[j]] = [array[j], array[i]]
   }
 }
-
-/** @description Randomized Priority Queue Selection Strategy */
 export const randomizedStrategy = (filteredEvents: CandidateBid[]) => {
   shuffle(filteredEvents)
   return filteredEvents.sort(
     ({priority: priorityA}, {priority: priorityB}) => priorityA - priorityB,
   )[0]
 }
+
 /** @description Chaos Selection Strategy */
-export const chaosStrategy = (filteredEvents: CandidateBid[]) => {
-  const randomArrayElement = (arr: CandidateBid[]) =>
-    arr[Math.floor(Math.random() * Math.floor(arr.length))]
-  return randomArrayElement(filteredEvents)
-}
+const randomArrayElement = (arr: CandidateBid[]) =>
+  arr[Math.floor(Math.random() * Math.floor(arr.length))]
+export const chaosStrategy = (filteredEvents: CandidateBid[]) => randomArrayElement(filteredEvents)
+
 /** @description Priority Queue Selection Strategy */
-export const priorityStrategy = (filteredEvents: CandidateBid[]) => {
-  return filteredEvents.sort(
-    ({priority: priorityA}, {priority: priorityB}) =>
-      priorityA - priorityB,
-  )[0]
-}
+export const priorityStrategy = (filteredEvents: CandidateBid[]) => filteredEvents.sort(
+  ({priority: priorityA}, {priority: priorityB}) =>
+    priorityA - priorityB,
+)[0]
